b98f18efbd864b44a444df5ab22ff04262a5901d
sshipp57 <sshipp57@Illumiscotty>
2016-10-05 09:36:22 -0600

    10-5-16 Added comment

--- Stranger Things/src/stranger/things/StrangerThings.java
+++ Stranger Things/src/stranger/things/StrangerThings.java
@@ -16,6 +16,9 @@ public class StrangerThings {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        // Create instances for every class
     }
     
 }

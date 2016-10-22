5e3920c9ec16a30c4e527197379fb19efb2207fb
sshipp57 <sshipp57@Illumiscotty>
2016-10-22 13:16:08 -0600

    10-22-16 MapControl

---
+++ Stranger Things/src/byui/cit260/strangerThings/control/MapControl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.strangerThings.control;
+
+/**
+ *
+ * @author sshipp57
+ */
+public class MapControl {
+    
+    public double calcRunningAway (double finalvelocity, double initialvelocity, double time){
+    
+        if (finalvelocity <= 0) {
+                return -1;
+                        }
+        if (initialvelocity <= 0 || initialvelocity > 15) {
+                return -1;
+                        }
+        if (time <= 0) {
+                return -1;
+        }
+        
+        
+         double acceleration = (finalvelocity - initialvelocity) / time;
+         
+         
+         return acceleration;
+    }
+    
+    
+}

b50de0cd8571bbea07020b1913b2afe26c0e47d6
sshipp57 <sshipp57@Illumiscotty>
2016-10-29 15:07:29 -0600

    10-29-16 StartProgramView, GameControl, MainMenuView

---
+++ Stranger Things/src/byui/cit260/strangerThings/control/GameControl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.strangerThings.control;
+
+import byui.cit260.strangerThings.model.Player;
+import stranger.things.StrangerThings;
+
+/**
+ *
+ * @author sshipp57
+ */
+public class GameControl {
+
+    public static Player createPlayer(String name) {
+        
+        if (name == null) {
+            return null;
+        }
+        
+        Player player = new Player();
+        player.setName(name);
+        
+        StrangerThings.setPlayer(player);
+        
+        return player;
+    }
+    
+}

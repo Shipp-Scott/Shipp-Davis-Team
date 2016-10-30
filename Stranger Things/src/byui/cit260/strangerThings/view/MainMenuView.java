4477e2d7a8e7776eb089aaeda91cf89cd47757ef
sshipp57 <sshipp57@Illumiscotty>
2016-10-29 18:57:02 -0600

    10-29-16 StartProgramView, GameControl, MainMenuView

--- Stranger Things/src/byui/cit260/strangerThings/view/MainMenuView.java
+++ Stranger Things/src/byui/cit260/strangerThings/view/MainMenuView.java
@@ -5,14 +5,108 @@
  */
 package byui.cit260.strangerThings.view;
 
+import byui.cit260.strangerThings.control.GameControl;
+import java.util.Scanner;
+import stranger.things.StrangerThings;
+
 /**
  *
  * @author sshipp57
  */
 public class MainMenuView {
+    
+   private final String menu;
+   
+   public MainMenuView(){
+    this.menu = "\n"
+              + "\n--------------------------------"
+              + "\n|  Main Menu                   |"
+              + "\n--------------------------------"
+              + "\nN - New Game                    "
+              + "\nS - Save Game                   "
+              + "\nR - Restore Existing Game       "
+              + "\nH - Help Menu                   "
+              + "\nE - Exit Game                   "
+              + "\n--------------------------------";
+   }
+    
+   
+    public void displayMainMenuView() {
+ 
+        boolean done = false;
+        do {
+            
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                return;
+            
+            done = this.doAction(menuOption);
+        } while (!done);
+    }
 
-    void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called ***");
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+        
+        while (!valid) {
+            System.out.println("\n" + this.menu);
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length() < 1) {
+                System.out.println("\nInvaild value: value can not be blank");
+                continue;
+            }
+            
+            break;
+           }
+        return value;
+    }
+
+    private boolean doAction(String choice) {
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "N":
+                this.startNewGame();
+                break;
+            case "S":
+                this.saveGame();
+                break;
+            case "R":
+                this.startExistingGame();
+                break;
+            case "H":
+                this.displayHelpMenu();
+                break;
+            default:
+                System.out.println("\n*** Invaild selection *** Try again");
+                break;
+                
+        }
+        return false;
+    }
+
+    private void startNewGame() {
+        
+        GameControl.createNewGame(StrangerThings.getPlayer());
+        
+        GameMenuView gameMenu = new GameMenuView();
+        gameMenu.displayMenu();
+    }
+
+    private void saveGame() {
+        System.out.println("*** saveGame function called");
+    }
+
+    private void startExistingGame() {
+        System.out.println("*** startExistingGame function called");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** displayHelpMenu function called");
     }
     
 }

b26652ea580884cdf653e2ea7b3128f4654270c4
Cache <Cache@Cache>
2016-12-13 11:11:48 -0700

    12/5/2016 Lesson 11 Team Assignment

--- Stranger Things/src/stranger/things/StrangerThings.java
+++ Stranger Things/src/stranger/things/StrangerThings.java
@@ -8,6 +8,10 @@ package stranger.things;
 import byui.cit260.strangerThings.model.Game;
 import byui.cit260.strangerThings.model.Player;
 import byui.cit260.strangerThings.view.StartProgramView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 /**
  *
@@ -16,7 +20,13 @@ import byui.cit260.strangerThings.view.StartProgramView;
 public class StrangerThings {
 
     private static Game currentGame = null;
-
+    private static Player player = null;
+    
+    private static PrintWriter outFile = null;
+    private static BufferedReader inFile = null;
+ 
+    private static PrintWriter logFile = null;
+    
     public static Game getCurrentGame() {
         return currentGame;
     }
@@ -32,15 +42,72 @@ public class StrangerThings {
     public static void setPlayer(Player player) {
         StrangerThings.player = player;
     }
-    private static Player player = null;
+    
+       public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        StrangerThings.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        StrangerThings.inFile = inFile;
+    }
+    
+    public static PrintWriter getLogFile(){
+        return logFile;
+    }
+    
+    public static void setLogFile(PrintWriter logFile) {
+        StrangerThings.logFile = logFile;
+    }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       // create StartProgramViewOrig and display the start program view
+       
+        try {
+            
+            //open character stream files for end user input and output
+            StrangerThings.inFile =
+                    new BufferedReader(new InputStreamReader(System.in));
+            
+            StrangerThings.outFile = new PrintWriter(System.out, true);
+       
+            //open log file
+            String filePath = "log.txt";
+            StrangerThings.logFile = new PrintWriter(filePath);
+            
+        // create StartProgramViewOrig and display the start program view
         StartProgramView startProgramView = new StartProgramView();
         startProgramView.displayStartProgramView();
+    } catch (Throwable e) {
+        
+        System.out.println("Exception: " + e.toString() +
+                           "\nCause: " + e.getCause() +
+                           "\nMessage: " + e.getMessage());
+    }
+        finally {
+            try {
+                if (StrangerThings.inFile != null)
+                    StrangerThings.inFile.close();
+                
+                if (StrangerThings.outFile != null)
+                    StrangerThings.outFile.close();
+                
+                if (StrangerThings.logFile != null)
+                    StrangerThings.logFile.close();
+            } catch (IOException ex) {
+                System.out.println("Error closing files");
+                return;
+            }
+        }
     }
     
 }
